824d6f79abb37eb8945868456edadb0748abb0fd
Neema <neema.edward@student.moringaschool.com>
2025-07-17 17:05:21 +0300

    adds imports

---
+++ server/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from config import Config
+from models.user import User, db, bcrypt
+from models.order import Order
+

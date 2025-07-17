3825bacc1c068e866e3717e61af02639f96e9d8f
Neema <neema.edward@student.moringaschool.com>
2025-07-17 17:09:07 +0300

    Initializes extensions and database

--- server/app.py
+++ server/app.py
@@ -6,3 +6,12 @@ from config import Config
 from models.user import User, db, bcrypt
 from models.order import Order
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+bcrypt.init_app(app)
+jwt = JWTManager(app)
+
+with app.app_context():
+    db.create_all()

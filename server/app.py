9ce234174462b27ccb33b94ac6b6a4b67954e64b
titophil <kipronotitus254@gmail.com>
2025-07-18 05:29:11 +0300

    all

--- server/app.py
+++ server/app.py
@@ -1,16 +1,21 @@
 from flask import Flask,jsonify
+from flask import Flask
+from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from config import Config
-from routes.Menu import menu_bp
+from extensions import db, migrate, jwt
+
 from routes.admin_routes import admin_bp
 from routes.payment_routes import payment_bp
-from extensions import db, migrate, jwt
+from extensions import db  
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+migrate = Migrate()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
@@ -27,17 +32,28 @@ def create_app():
     app.config['DARAJA_CONSUMER_SECRET'] = os.getenv('DARAJA_CONSUMER_SECRET')
     app.config['DARAJA_SHORTCODE'] = os.getenv('DARAJA_SHORTCODE')
 
-   
+    # Init extensions
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
     CORS(app)
 
+    # Register Blueprints (empty for now)
+    # from routes.auth import auth_bp
+    # app.register_blueprint(auth_bp, url_prefix='/auth')
+    @app.route('/')
+    def home():
+        return jsonify({"message": "Welcome to Mealy API!"})
+
+
+
+
+    from routes.Menu import menu_bp
+    app.register_blueprint(menu_bp,url_prefix='/menus')
+
     # Register Blueprints
     app.register_blueprint(admin_bp, url_prefix="/admin")
-    app.register_blueprint(menu_bp,url_prefix='/menus')
     app.register_blueprint(payment_bp)
-
 
     return app  # ✅ Make sure to return the app!
 

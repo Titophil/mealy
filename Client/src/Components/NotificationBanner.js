8c16f40a6b96069252d5c7c435eae5b9ff78606a
moreen karimi <moreenk541@gmail.com>
2025-07-21 18:14:14 +0300

    New menu alert

--- Client/src/Components/NotificationBanner.js
+++ Client/src/Components/NotificationBanner.js
@@ -3,3 +3,33 @@ import { getMenuByDate } from '../api/menuApi';
 
 function NotificationBanner() {
   const [show, setShow] = useState(false);
+
+    useEffect(() => {
+    const checkMenu = async () => {
+      const today = new Date().toISOString().split('T')[0];
+      try {
+        const res = await getMenuByDate(today);
+        if (res.data) setShow(true);
+      } catch (e) {
+        setShow(false);
+      }
+    };
+
+    const interval = setInterval(checkMenu, 60000); 
+    checkMenu(); 
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!show) return null;
+
+  return (
+    <div style={{ background: 'lightgreen', padding: '10px' }}>
+        Today’s menu is live!
+      <button onClick={() => setShow(false)} style={{ float: 'right' }}>X</button>
+    </div>
+  );
+}
+
+export default NotificationBanner;
+

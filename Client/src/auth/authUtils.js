5fc4be84cfa7a2e71b435f51aac8b43f833d2308
Neema <neema.edward@student.moringaschool.com>
2025-07-22 10:45:50 +0300

    Adds functions to manage JWT token in local storage

---
+++ Client/src/auth/authUtils.js
@@ -0,0 +1,10 @@
+const TOKEN_KEY = 'jwtToken';
+
+export const setToken = (token) => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem(TOKEN_KEY);
+};
+

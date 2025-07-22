ed34483b6516d41810098cc3779903c8d819f274
Sam <ssamsanya3@gmail.com>
2025-07-22 14:35:16 +0300

    Meal List

---
+++ Client/src/Components/DeleteMealModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeleteMealModal = () => {
+  return (
+    <div>
+      <h2>Delete Meal Confirmation</h2>
+      {/* Implement delete meal confirmation modal here */}
+    </div>
+  );
+};
+
+export default DeleteMealModal;
--- Client/src/Routes.jsx
+++ Client/src/Routes.jsx
@@ -4,6 +4,8 @@ import Overview from "./Components/Overview";
 import OrdersCard from "./Components/OrdersCard";
 import Meals from "./Components/Meals";
 import Revenue from "./Components/Revenue";
+import MealList from "./Components/MealList";
+import MealForm from "./Components/MealForm";
 import React from "react";
 
 export const appRoutes = [
@@ -17,7 +19,8 @@ export const appRoutes = [
       },
       { path: "overview", element: <Overview /> },
       { path: "orders", element: <OrdersCard /> },
-      { path: "meals", element: <Meals /> },
+      { path: "meals", element: <MealList /> },
+      { path: "meals/add", element: <MealForm /> },
       { path: "revenue", element: <Revenue /> },
     ],
   },

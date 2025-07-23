cb14fcf0678b8103297f0ca70c293982a7a3ba28
titophil <kipronotitus254@gmail.com>
2025-07-23 12:17:42 +0300

    all

--- Client/src/Routes.jsx
+++ Client/src/Routes.jsx
@@ -1,25 +1,41 @@
+import React from "react";
 import NotFound from "./Components/NotFound";
 import Admin from "./Pages/Admin";
 import Overview from "./Components/Overview";
 import OrdersCard from "./Components/OrdersCard";
 import Meals from "./Components/Meals";
 import Revenue from "./Components/Revenue";
-import React from "react";
+import OrderForm from "./Components/OrderForm";       
+import TodaysOrder from "./Components/TodaysOrder";   
+import PrivateRoute from "./Components/PrivateRoute"; 
 
 export const appRoutes = [
   {
     path: "/admin",
     element: <Admin />,
     children: [
-      {
-        index: true, // 👈 This makes /admin default to Overview
-        element: <Overview />
-      },
+      { index: true, element: <Overview /> },
       { path: "overview", element: <Overview /> },
       { path: "orders", element: <OrdersCard /> },
       { path: "meals", element: <Meals /> },
       { path: "revenue", element: <Revenue /> },
     ],
+  },
+  {
+    path: "/order",
+    element: (
+      <PrivateRoute>
+        <OrderForm />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/order/current",
+    element: (
+      <PrivateRoute>
+        <TodaysOrder />
+      </PrivateRoute>
+    ),
   },
   {
     path: "*",

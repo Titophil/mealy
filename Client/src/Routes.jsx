0330ac36eff98da000a01427b52e333be165d594
titophil <kipronotitus254@gmail.com>
2025-07-23 13:06:31 +0300

    all

--- Client/src/Routes.jsx
+++ Client/src/Routes.jsx
@@ -1,47 +1,55 @@
 import React from "react";
-import NotFound from "./Components/NotFound";
+import { Routes, Route } from "react-router-dom";
+import PublicLandingPage from "./pages/public/PublicLandingPage.jsx";
+import LoginPage from "./pages/public/LoginPage.jsx";
+import SignupPage from "./pages/public/SignupPage.jsx";
+
 import Admin from "./Pages/Admin";
 import Overview from "./Components/Overview";
 import OrdersCard from "./Components/OrdersCard";
 import Meals from "./Components/Meals";
 import Revenue from "./Components/Revenue";
-import OrderForm from "./Components/OrderForm";
-import TodaysOrder from "./Components/TodaysOrder";
-import PrivateRoute from "./Components/PrivateRoute";
 import MealList from "./Components/MealList";
 import MealForm from "./Components/MealForm";
 
-export const appRoutes = [
-  {
-    path: "/admin",
-    element: <Admin />,
-    children: [
-      { index: true, element: <Overview /> },
-      { path: "overview", element: <Overview /> },
-      { path: "orders", element: <OrdersCard /> },
-      { path: "meals", element: <MealList /> },
-      { path: "meals/add", element: <MealForm /> },
-      { path: "revenue", element: <Revenue /> },
-    ],
-  },
-  {
-    path: "/order",
-    element: (
-      <PrivateRoute>
-        <OrderForm />
-      </PrivateRoute>
-    ),
-  },
-  {
-    path: "/order/current",
-    element: (
-      <PrivateRoute>
-        <TodaysOrder />
-      </PrivateRoute>
-    ),
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-];
+import OrderForm from "./Components/OrderForm";
+import TodaysOrder from "./Components/TodaysOrder";
+import PrivateRoute from "./Components/PrivateRoute";
+
+import UserDashboard from "./pages/user/UserDashboard.jsx";
+import OrderHistory from "./pages/user/OrderHistory.jsx";
+import NotFound from "./Components/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* Public Pages */}
+      <Route path="/" element={<PublicLandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* Admin Pages */}
+      <Route path="/admin" element={<Admin />}>
+        <Route index element={<Overview />} />
+        <Route path="overview" element={<Overview />} />
+        <Route path="orders" element={<OrdersCard />} />
+        <Route path="meals" element={<MealList />} />
+        <Route path="meals/add" element={<MealForm />} />
+        <Route path="revenue" element={<Revenue />} />
+      </Route>
+
+      {/* User Private Routes */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="/user/orders" element={<OrderHistory />} />
+        <Route path="/order" element={<OrderForm />} />
+        <Route path="/order/current" element={<TodaysOrder />} />
+      </Route>
+
+      {/* 404 Fallback */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;

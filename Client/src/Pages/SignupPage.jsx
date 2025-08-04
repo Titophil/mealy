ffe841697dd9598f2c5f1ec3cac74921994db750
titophil <kipronotitus254@gmail.com>
2025-08-04 09:34:50 +0300

    all

--- Client/src/Pages/SignupPage.jsx
+++ Client/src/Pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../Components/AuthContext';
+import { useAuth } from '../auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
